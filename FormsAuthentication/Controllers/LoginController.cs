daab236e4cf1033710546d426b0901bdac8622c8
JeanLLopes <jeanllopes@outlook.com>
2016-11-22 21:23:22 -0200

    Initial Commit

---
+++ FormsAuthentication/Controllers/LoginController.cs
@@ -0,0 +1,34 @@
+﻿using AAFormsAuthentication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+//Para se autenticar via FormsAuthentication
+//Temos que usar o seguinte Namespace:
+using System.Web.Security;
+
+namespace AAFormsAuthentication.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [ValidateAntiForgeryToken] //Validando a origem.
+        [HttpPost]
+        public ActionResult Index(LoginModel login, string returnUrl)
+        {
+            return View();
+        }
+
+        public ActionResult LogOff() {
+            
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ FormsAuthentication/Global.asax.cs
@@ -0,0 +1,47 @@
+﻿using AAFormsAuthentication.App_Start;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.Security;
+
+namespace AAFormsAuthentication
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            //Habilitando o Filtro para aceitar apenas requisições
+            //Autenticadas
+            FilterConfig.RouteConfig(GlobalFilters.Filters);
+        }
+        //Evento que permite criar uma role do usuário
+        //Como funcionava antes do OWIN
+        public void Application_OnPostAuthorizeRequest()
+        {
+            var cookie = FormsAuthentication.FormsCookieName;
+
+            if (cookie == null)
+                return;
+
+            HttpCookie httpCookie = Context.Request.Cookies[cookie];
+
+            if (httpCookie == null)
+                return;
+
+            FormsAuthenticationTicket ticket = FormsAuthentication.Decrypt(httpCookie.Value);
+
+            FormsIdentity identity = new FormsIdentity(ticket);
+
+            GenericPrincipal principal = new GenericPrincipal(identity, new[] { "Administrador" });
+
+            HttpContext.Current.User = principal;
+        }
+    }
+}
